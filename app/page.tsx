4672131a0f69a30dfbc7a2dc62ad7fb35978240f
cnngts <xxxxxqq@me.com>
2025-06-02 18:38:35 +0800

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>📰 NewsNow 启动成功！</h1>
+      <p>这是你的首页，可以开始自定义内容了。</p>
+    </main>
+  );
+}
