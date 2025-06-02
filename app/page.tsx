0f261d1cbb4ed3ec89c5da99df47c53649154568
cnngts <xxxxxqq@me.com>
2025-06-02 18:55:49 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>📰 NewsNow 启动成功！</h1>
-      <p>这是你的首页，可以开始自定义内容了。</p>
+    <main>
+      <h1>Hello from NewsNow!</h1>
+      <p>这是你部署成功的首页。</p>
     </main>
   );
 }
